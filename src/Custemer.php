7394a8790144bfd7306684113f7d974f01360df7
AMIPA UMADINIAH <nyaaaku5@gamil.com>
2023-05-30 00:27:13 +0700

    Conotoh Belajar Php Composer

---
+++ src/Custemer.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace MipaLearn;
+
+
+class Custemer 
+{
+    public function __construct(private string $name)
+    {
+        
+    }
+    public function sayHello(string $name): string
+{
+    return "Hello $name, My Name is $this->$name";
+}
+
+
+
+}
+
+
+
+?>
